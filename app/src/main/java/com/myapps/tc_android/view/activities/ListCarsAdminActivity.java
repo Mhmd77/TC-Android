afe85eb9a7d3632604f0386eae9a87882def9a57
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-11-22 10:43:53 +0330

    feat : Update listcars admin
    Add onclick button for update

--- app/src/main/java/com/myapps/tc_android/view/activities/ListCarsAdminActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/ListCarsAdminActivity.java
@@ -1,6 +1,7 @@
 package com.myapps.tc_android.view.activities;
 
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -81,7 +82,10 @@ public class ListCarsAdminActivity extends AppCompatActivity implements Callback
 
             @Override
             public void updateOnClick(View view,int position) {
-                //TODO
+                Car car =  cars.get(position);
+                Intent intent = new Intent(ListCarsAdminActivity.this,UpdateCarAdminActivity.class);
+                intent.putExtra("Car",car);
+                startActivity(intent);
 
             }
 
