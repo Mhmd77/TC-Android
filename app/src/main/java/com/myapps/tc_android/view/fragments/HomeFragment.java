d8b4d99ba733123a413bb5e6e242ca3576febf84
Mhmd77 <m.emami9@yahoo.com>
2018-12-04 17:53:48 +0330

    feat: Add HomeFragment logic

--- app/src/main/java/com/myapps/tc_android/view/fragments/HomeFragment.java
+++ app/src/main/java/com/myapps/tc_android/view/fragments/HomeFragment.java
@@ -1,109 +1,197 @@
 package com.myapps.tc_android.view.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.github.ybq.android.spinkit.SpinKitView;
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
 import com.myapps.tc_android.R;
+import com.myapps.tc_android.controller.Utils;
+import com.myapps.tc_android.controller.adapter.CarsRecyclerView;
+import com.myapps.tc_android.controller.network.ApiService;
+import com.myapps.tc_android.controller.network.RetrofitClientInstance;
+import com.myapps.tc_android.model.ApiResponse;
+import com.myapps.tc_android.model.Car;
+import com.myapps.tc_android.view.activities.AddCarUserActivity;
+import com.myapps.tc_android.view.activities.CarProfileActivity;
+import com.myapps.tc_android.view.activities.HomePageActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link HomeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
-public class HomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-    private OnFragmentInteractionListener mListener;
+public class HomeFragment extends Fragment implements Callback<ApiResponse<List<Car>>>, CarsRecyclerView.UserOnItemClickListener, View.OnClickListener {
+    @BindView(R.id.recyclerView_main_cars)
+    RecyclerView recyclerViewMainCars;
+    @BindView(R.id.radioButtonSortAscending)
+    RadioButton radioButtonSortAscending;
+    @BindView(R.id.radioButtonSortDescending)
+    RadioButton radioButtonSortDescending;
+    @BindView(R.id.actionButton_main_add_car)
+    FloatingActionButton actionButtonMainAddCar;
+    @BindView(R.id.spinnerLoading)
+    SpinKitView spinnerLoading;
+    private CarsRecyclerView adapter;
+    private ApiService service;
+    @BindView(R.id.sortbar)
+    View sortBar;
+    private String field;
+    private int ascending = 1;
+    private Unbinder unbinder;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
     public HomeFragment() {
-        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        service = RetrofitClientInstance.getRetrofitInstance().create(ApiService.class);
+        updateList();
+        initSortBar();
+        initSpinner();
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    private void initSpinner() {
+        Sprite doubleBounce = new DoubleBounce();
+        spinnerLoading.setIndeterminateDrawable(doubleBounce);
+    }
+
+    private void initSortBar() {
+        ((HomePageActivity) getActivity()).buttonSortCost.setOnClickListener(this);
+        ((HomePageActivity) getActivity()).buttonSortYear.setOnClickListener(this);
+        Utils.collapse(sortBar);
+        ((RadioGroup) sortBar).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButtonSortDescending:
+                        ascending = 0;
+                        break;
+                    case R.id.radioButtonSortAscending:
+                        ascending = 1;
+                        break;
+                }
+            }
+        });
+    }
+
+    private void updateList() {
+        Call<ApiResponse<List<Car>>> call = service.getAllCars();
+        call.enqueue(this);
+    }
+
+    private void generateDataList(final List<Car> cars) {
+        adapter = new CarsRecyclerView(getActivity(), cars, this);
+        recyclerViewMainCars.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerViewMainCars.setAdapter(adapter);
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+    public void onResponse(@NonNull Call<ApiResponse<List<Car>>> call, @NonNull Response<ApiResponse<List<Car>>> response) {
+        if (response.body() != null) {
+            generateDataList(response.body().getObject());
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            Log.e("CarList", "response body is null");
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onFailure(@NonNull Call<ApiResponse<List<Car>>> call, @NonNull Throwable t) {
+        Toast.makeText(getActivity(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+
+    @OnClick(R.id.actionButton_main_add_car)
+    public void onViewClicked() {
+        startActivity(new Intent(getActivity(), AddCarUserActivity.class));
+    }
+
+    @Override
+    public void cardOnClick(View view, int position) {
+        Car car = adapter.getList().get(position);
+        Intent intent = new Intent(getActivity(), CarProfileActivity.class);
+        intent.putExtra("Car", car);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.buttonSortCars)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSortYear:
+                Utils.expand(sortBar);
+                field = "year";
+                break;
+            case R.id.buttonSortCost:
+                Utils.expand(sortBar);
+                field = "price";
+                break;
+            case R.id.buttonSortCars:
+                spinnerLoading.setVisibility(View.VISIBLE);
+                Call<ApiResponse<List<Car>>> call = service.sortCars(field, ascending);
+                call.enqueue(new Callback<ApiResponse<List<Car>>>() {
+                    @Override
+                    public void onResponse(Call<ApiResponse<List<Car>>> call, Response<ApiResponse<List<Car>>> response) {
+                        spinnerLoading.setVisibility(View.GONE);
+                        if (response.isSuccessful()) {
+                            if (response.body().getStatus().equals("OK")) {
+                                adapter.setList(response.body().getObject());
+                                Utils.collapse(sortBar);
+                            } else {
+                                Log.e("Sort Error", " status : " + response.body().getStatus());
+                            }
+                        } else {
+                            Log.e("Sort Error", " error code: " + response.code());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ApiResponse<List<Car>>> call, Throwable t) {
+                        Toast.makeText(getActivity(), "Something went wrong. please try again!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
