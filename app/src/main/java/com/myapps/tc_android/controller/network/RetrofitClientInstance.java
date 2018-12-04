9ad7cedb53eb8922aab76027630135581dc62799
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-04 15:50:16 +0330

    fix: Update add car UI

--- app/src/main/java/com/myapps/tc_android/controller/network/RetrofitClientInstance.java
+++ app/src/main/java/com/myapps/tc_android/controller/network/RetrofitClientInstance.java
@@ -14,7 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://172.17.11.7:5000/";
+    private static final String BASE_URL = "http://172.20.10.9:5000/";
 
     public static Retrofit getRetrofitInstance() {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
