9d7965d6058ef120bc610cd56b5096cd712df6ff
Mhmd77 <m.emami9@yahoo.com>
2018-12-04 17:55:07 +0330

    fix: Update Home Activity logic

--- app/src/main/java/com/myapps/tc_android/view/activities/HomePageActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/HomePageActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.myapps.tc_android.R;
+import com.myapps.tc_android.view.fragments.HomeFragment;
+import com.myapps.tc_android.view.fragments.RentFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,36 +26,59 @@ public class HomePageActivity extends AppCompatActivity implements BottomNavigat
     FrameLayout frameLayoutContainer;
     @BindView(R.id.bottom_navigation_menu_home_home)
     BottomNavigationView bottomNavigationMenuHomeHome;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.buttonSortYear)
+    public Button buttonSortYear;
+    @BindView(R.id.buttonSortCost)
+    public Button buttonSortCost;
+    private Fragment homeFragment, rentFragment, carFragment, active;
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        initFragmnets();
         bottomNavigationMenuHomeHome.setOnNavigationItemSelectedListener(this);
+        bottomNavigationMenuHomeHome.setSelectedItemId(R.id.navigation_home);
+    }
+
+    private void initFragmnets() {
+        fm = getSupportFragmentManager();
+        homeFragment = HomeFragment.newInstance();
+        rentFragment = RentFragment.newInstance();
+        carFragment = RentFragment.newInstance();
+        active = homeFragment;
+        fm.beginTransaction().add(R.id.frame_layout_container, carFragment, "3").hide(carFragment).commit();
+        fm.beginTransaction().add(R.id.frame_layout_container, rentFragment, "2").hide(rentFragment).commit();
+        fm.beginTransaction().add(R.id.frame_layout_container, homeFragment, "1").commit();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment fragment;
         switch (item.getItemId()) {
             case R.id.navigation_home:
-                getSupportActionBar().setTitle("HOME");
+                fm.beginTransaction().hide(active).show(homeFragment).commit();
+                active = homeFragment;
+                buttonSortYear.setVisibility(View.VISIBLE);
+                buttonSortCost.setVisibility(View.VISIBLE);
                 return true;
             case R.id.navigation_rent:
-                getSupportActionBar().setTitle("RENT");
+                fm.beginTransaction().hide(active).show(rentFragment).commit();
+                active = rentFragment;
+                buttonSortYear.setVisibility(View.INVISIBLE);
+                buttonSortCost.setVisibility(View.INVISIBLE);
                 return true;
             case R.id.navigation_cars:
-                getSupportActionBar().setTitle("CARS");
+                fm.beginTransaction().hide(active).show(rentFragment).commit();
+                active = rentFragment;
+                buttonSortYear.setVisibility(View.INVISIBLE);
+                buttonSortCost.setVisibility(View.INVISIBLE);
                 return true;
         }
         return false;
-    }
-
-    private void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout_container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
     }
 }
