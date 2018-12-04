10c9a954f9e90c0ec05b4299df6d813bea8d8fd9
Mhmd77 <m.emami9@yahoo.com>
2018-12-04 15:35:32 +0330

    feat: Add bottom menu view
    Add menu file
    Update layout file
    Update java file
    Update string files
    Update styles files
    Add image files

--- app/src/main/java/com/myapps/tc_android/view/activities/HomePageActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/HomePageActivity.java
@@ -1,20 +1,55 @@
 package com.myapps.tc_android.view.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.myapps.tc_android.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    @BindView(R.id.frame_layout_container)
+    FrameLayout frameLayoutContainer;
+    @BindView(R.id.bottom_navigation_menu_home_home)
+    BottomNavigationView bottomNavigationMenuHomeHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         ButterKnife.bind(this);
+        bottomNavigationMenuHomeHome.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                getSupportActionBar().setTitle("HOME");
+                return true;
+            case R.id.navigation_rent:
+                getSupportActionBar().setTitle("RENT");
+                return true;
+            case R.id.navigation_cars:
+                getSupportActionBar().setTitle("CARS");
+                return true;
+        }
+        return false;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
