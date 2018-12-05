c9eeeaadca5f24822f89692eb150f77590e7dd64
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-05 15:24:17 +0330

    feat : Add home page admin activity
    Add activity to manifest

---
+++ app/src/main/java/com/myapps/tc_android/view/activities/HomePageAdminActivity.java
@@ -0,0 +1,15 @@
+package com.myapps.tc_android.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myapps.tc_android.R;
+
+public class HomePageAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_admin);
+    }
+}
