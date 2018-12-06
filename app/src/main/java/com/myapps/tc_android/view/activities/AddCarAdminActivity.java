6689cddc427aedde0085b9f2badcb3654aee9643
Mhmd77 <m.emami9@yahoo.com>
2018-12-06 16:37:07 +0330

    feat: Add upload image to 'AddCarAdmin'

--- app/src/main/java/com/myapps/tc_android/view/activities/AddCarAdminActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/AddCarAdminActivity.java
@@ -1,7 +1,13 @@
 package com.myapps.tc_android.view.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,9 +25,18 @@ import com.myapps.tc_android.controller.network.RetrofitClientInstance;
 import com.myapps.tc_android.model.ApiResponse;
 import com.myapps.tc_android.model.Car;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
+import in.mayanknagwanshi.imagepicker.imageCompression.ImageCompressionListener;
+import in.mayanknagwanshi.imagepicker.imagePicker.ImagePicker;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -44,12 +59,19 @@ public class AddCarAdminActivity extends AppCompatActivity implements Callback<A
     EditText editTextAddCarPrice;
     @BindView(R.id.button_addCar)
     Button buttonAddCar;
+    @BindView(R.id.imageview_previewImage)
+    CircleImageView imageviewPreviewImage;
+    private ImagePicker imagePicker;
+    private String TAG = AddCarAdminActivity.class.getSimpleName();
+    private File image;
+    private Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_car_admin);
         ButterKnife.bind(this);
+        imagePicker = new ImagePicker();
     }
 
     @OnClick(R.id.button_addCar)
@@ -147,9 +169,14 @@ public class AddCarAdminActivity extends AppCompatActivity implements Callback<A
     public void onResponse(@NonNull Call<ApiResponse<Car>> call, @NonNull Response<ApiResponse<Car>> response) {
         if (response.isSuccessful()) {
             if (response.body().getStatus().equals("OK")) {
-                Toast.makeText(AddCarAdminActivity.this, "Car Added Successfully ", Toast.LENGTH_SHORT).show();
-                finish();
-                startActivity(new Intent(AddCarAdminActivity.this, ListCarsAdminActivity.class));
+                car = response.body().getObject();
+                if (image != null) {
+                    sendPhoto();
+                } else {
+                    finish();
+                    startActivity(new Intent(AddCarAdminActivity.this, ListCarsAdminActivity.class));
+                }
+                Toast.makeText(AddCarAdminActivity.this, car.getName() + " Added Successfully ", Toast.LENGTH_SHORT).show();
             } else {
                 Log.e("Add Car Error", " status : " + response.body().getStatus());
             }
@@ -162,5 +189,91 @@ public class AddCarAdminActivity extends AppCompatActivity implements Callback<A
     public void onFailure(@NonNull Call<ApiResponse<Car>> call, @NonNull Throwable t) {
         Log.e("Connection", "Failed To Connect : " + t.getMessage());
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        if (requestCode == ImagePicker.SELECT_IMAGE && resultCode == RESULT_OK) {
+            imagePicker.addOnCompressListener(new ImageCompressionListener() {
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void onCompressed(String filePath) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                    imageviewPreviewImage.setImageBitmap(bitmap);
+                    image = new File(filePath);
+                }
+            });
+        }
+        String filePath = imagePicker.getImageFilePath(data);
+        if (filePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+            imageviewPreviewImage.setImageBitmap(bitmap);
+            image = new File(filePath);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @OnClick(R.id.button_chooseImage)
+    public void onPickImage() {
+        if (isStoragePermissionGranted()) {
+            imagePicker.withActivity(this)
+                    .withCompression(false)
+                    .start();
+        }
+    }
+
+    public boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG, "Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG, "Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        } else {
+            Log.v(TAG, "Permission is granted");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+            onPickImage();
+        }
+    }
+
+    private void sendPhoto() {
+        ApiService service = RetrofitClientInstance.getRetrofitInstance().create(ApiService.class);
+        RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), image);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("file", image.getName(), reqFile);
+        Call<ResponseBody> req = service.uploadImage(body, car.getId());
+        req.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.i(TAG, "image " + image.getName() + " uploaded successfully");
+                    finish();
+                    startActivity(new Intent(AddCarAdminActivity.this, ListCarsAdminActivity.class));
+                } else {
+                    Log.e(TAG, "uploading image failed with code " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
