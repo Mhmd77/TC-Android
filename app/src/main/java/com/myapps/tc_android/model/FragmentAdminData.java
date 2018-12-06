d0ec8784a6f7a2c9e9de7ba7530b9da49a3aea5d
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-06 15:56:20 +0330

    feat: Add new data model for fragment

---
+++ app/src/main/java/com/myapps/tc_android/model/FragmentAdminData.java
@@ -0,0 +1,15 @@
+package com.myapps.tc_android.model;
+
+
+public class FragmentAdminData {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public FragmentAdminData(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
