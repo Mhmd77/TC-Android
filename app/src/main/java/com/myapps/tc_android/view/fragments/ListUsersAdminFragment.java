66941a8529fa046f0b170bd0c03c71c06590f572
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-07 12:12:52 +0330

    feat: Add list users admin fragment
    Add xml file
    Add java file

---
+++ app/src/main/java/com/myapps/tc_android/view/fragments/ListUsersAdminFragment.java
@@ -0,0 +1,108 @@
+package com.myapps.tc_android.view.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.myapps.tc_android.R;
+import com.myapps.tc_android.controller.adapter.CarsRecyclerView;
+import com.myapps.tc_android.controller.adapter.UserRecyclerView;
+import com.myapps.tc_android.controller.network.ApiService;
+import com.myapps.tc_android.controller.network.RetrofitClientInstance;
+import com.myapps.tc_android.model.ApiResponse;
+import com.myapps.tc_android.model.Car;
+import com.myapps.tc_android.model.User;
+import com.myapps.tc_android.view.activities.CarProfileActivity;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ListUsersAdminFragment extends Fragment implements Callback<ApiResponse<List<User>>>,UserRecyclerView.OnItemClickListener {
+    @BindView(R.id.recyclerView_main_users)
+    RecyclerView recyclerViewMainUsers;
+
+
+    private UserRecyclerView adapter;
+    private ApiService service;
+
+    private Unbinder unbinder;
+
+    public ListUsersAdminFragment() {
+    }
+
+    public static ListUsersAdminFragment newInstance() {
+        ListUsersAdminFragment fragment = new ListUsersAdminFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_users_admin, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        service = RetrofitClientInstance.getRetrofitInstance().create(ApiService.class);
+        updateList();
+
+        return view;
+    }
+
+    private void updateList() {
+        Call<ApiResponse<List<User>>> call = service.getAllUsers();
+        call.enqueue(this);
+    }
+
+    private void generateDataList(final List<User> users) {
+        adapter = new UserRecyclerView(getActivity(), users, this);
+        recyclerViewMainUsers.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerViewMainUsers.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResponse(@NonNull Call<ApiResponse<List<User>>> call, @NonNull Response<ApiResponse<List<User>>> response) {
+        if (response.body() != null) {
+            generateDataList(response.body().getObject());
+        } else {
+            Log.e("CarList", "response body is null");
+        }
+    }
+
+    @Override
+    public void onFailure(@NonNull Call<ApiResponse<List<User>>> call, @NonNull Throwable t) {
+        Toast.makeText(getActivity(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @Override
+    public void deleteOnclick(View itemView, int layoutPosition) {
+        //TODO
+    }
+}
