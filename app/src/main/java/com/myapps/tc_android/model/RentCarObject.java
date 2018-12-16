343cf0b20c8cb80d4cb9dacf55676d30c9787d53
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-16 19:03:16 +0330

    feat: Add rent car object

--- app/src/main/java/com/myapps/tc_android/model/RentCarObject.java
+++ app/src/main/java/com/myapps/tc_android/model/RentCarObject.java
@@ -1,4 +1,43 @@
 package com.myapps.tc_android.model;
 
+import java.sql.Date;
+
 public class RentCarObject {
+    private int carId;
+    private int userId;
+    private Date startDate;
+    private Date endDate;
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
 }
