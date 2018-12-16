47fe3178bc8641c34af8bdfa7124bcc7b79911ba
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-16 17:29:53 +0330

    feat: Add rent car activity
    Add activity to manifest
    Add java file

---
+++ app/src/main/java/com/myapps/tc_android/view/activities/RentCarActivity.java
@@ -0,0 +1,15 @@
+package com.myapps.tc_android.view.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myapps.tc_android.R;
+
+public class RentCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent_car);
+    }
+}
