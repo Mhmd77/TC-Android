7c197db4e4f8b7c419e3a53cf038520bebdf2d66
Mhmd77 <m.emami9@yahoo.com>
2018-12-18 00:19:30 +0330

    fix: Change ListCarsAdmin fragment logic

--- app/src/main/java/com/myapps/tc_android/view/fragments/ListCarsAdminFragment.java
+++ app/src/main/java/com/myapps/tc_android/view/fragments/ListCarsAdminFragment.java
@@ -1,8 +1,11 @@
 package com.myapps.tc_android.view.fragments;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,7 +23,7 @@ import com.myapps.tc_android.service.repository.ApiRepository;
 import com.myapps.tc_android.service.model.ApiResponse;
 import com.myapps.tc_android.service.model.Car;
 import com.myapps.tc_android.view.activities.CarProfileActivity;
-
+import com.myapps.tc_android.viewmodel.ListCarsViewModel;
 
 
 import java.util.List;
@@ -31,17 +34,19 @@ import butterknife.Unbinder;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-public class ListCarsAdminFragment extends Fragment implements Callback<ApiResponse<List<Car>>>, CarsRecyclerView.UserOnItemClickListener {
+
+public class ListCarsAdminFragment extends Fragment implements CarsRecyclerView.UserOnItemClickListener {
     @BindView(R.id.recyclerView_main_cars)
     RecyclerView recyclerViewMainCars;
 
 //    @BindView(R.id.actionButton_main_add_car)
-   // private FloatingActionButton actionButtonMainAddCar = (FloatingActionButton) getActivity().findViewById(R.id.actionButton_main_add_car);
+    // private FloatingActionButton actionButtonMainAddCar = (FloatingActionButton) getActivity().findViewById(R.id.actionButton_main_add_car);
 
     private CarsRecyclerView adapter;
     private ApiService service;
 
     private Unbinder unbinder;
+    private ListCarsViewModel viewModel;
 
     public ListCarsAdminFragment() {
     }
@@ -49,6 +54,22 @@ public class ListCarsAdminFragment extends Fragment implements Callback<ApiRespo
     public static ListCarsAdminFragment newInstance() {
         ListCarsAdminFragment fragment = new ListCarsAdminFragment();
         return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(ListCarsViewModel.class);
+        observeViewModel(viewModel);
+    }
+
+    private void observeViewModel(ListCarsViewModel viewModel) {
+        viewModel.getListCarsObservableData().observe(this, new Observer<List<Car>>() {
+            @Override
+            public void onChanged(@Nullable List<Car> cars) {
+                generateDataList(cars);
+            }
+        });
     }
 
     @Override
@@ -63,15 +84,8 @@ public class ListCarsAdminFragment extends Fragment implements Callback<ApiRespo
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_cars_admin, container, false);
         unbinder = ButterKnife.bind(this, view);
-        service = ApiRepository.getRetrofitInstance().create(ApiService.class);
-        updateList();
 
         return view;
-    }
-
-    private void updateList() {
-        Call<ApiResponse<List<Car>>> call = service.getAllCars();
-        call.enqueue(this);
     }
 
     private void generateDataList(final List<Car> cars) {
@@ -81,32 +95,13 @@ public class ListCarsAdminFragment extends Fragment implements Callback<ApiRespo
     }
 
     @Override
-    public void onResponse(@NonNull Call<ApiResponse<List<Car>>> call, @NonNull Response<ApiResponse<List<Car>>> response) {
-        if (response.body() != null) {
-            generateDataList(response.body().getObject());
-        } else {
-            Log.e("CarList", "response body is null");
-        }
-    }
-
-    @Override
-    public void onFailure(@NonNull Call<ApiResponse<List<Car>>> call, @NonNull Throwable t) {
-        Toast.makeText(getActivity(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-    }
-
-
-//    @OnClick(R.id.actionButton_main_add_car)
-//    public void onViewClicked() {
-//        startActivity(new Intent(getActivity(), AddCarAdminActivity.class));
-//    }
-
-    @Override
     public void cardOnClick(View view, int position) {
         int carId = adapter.getList().get(position).getId();
         Intent intent = new Intent(getActivity(), CarProfileActivity.class);
-        intent.putExtra("carId",carId);
+        intent.putExtra("carId", carId);
         startActivity(intent);
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
