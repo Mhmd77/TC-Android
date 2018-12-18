fd846a17d7522d532ebbabbe254a50cf5fb152ba
Mhmd77 <m.emami9@yahoo.com>
2018-12-18 22:26:04 +0330

    feat: Add is user attribute to view model

--- app/src/main/java/com/myapps/tc_android/viewmodel/ListCarsViewModel.java
+++ app/src/main/java/com/myapps/tc_android/viewmodel/ListCarsViewModel.java
@@ -3,6 +3,8 @@ package com.myapps.tc_android.viewmodel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
 
 import com.myapps.tc_android.service.model.Car;
 import com.myapps.tc_android.service.repository.ApiRepository;
@@ -12,8 +14,12 @@ import java.util.List;
 public class ListCarsViewModel extends ViewModel {
     private MutableLiveData<List<Car>> listCarsObservableData;
 
-    public ListCarsViewModel() {
-        this.listCarsObservableData = ApiRepository.getInstance().getListCars();
+    public ListCarsViewModel(boolean isUserCar) {
+        if (isUserCar) {
+            this.listCarsObservableData = ApiRepository.getInstance().getListCarUser();
+        } else {
+            this.listCarsObservableData = ApiRepository.getInstance().getListCars();
+        }
     }
 
     public LiveData<List<Car>> getListCarsObservableData() {
@@ -22,5 +28,19 @@ public class ListCarsViewModel extends ViewModel {
 
     public void sortListOfCars(String field, int ascending) {
         ApiRepository.getInstance().sortListCars(listCarsObservableData, field, ascending);
+    }
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+        private boolean isUserCar;
+
+        public Factory(boolean isUserCar) {
+            this.isUserCar = isUserCar;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new ListCarsViewModel(isUserCar);
+        }
     }
 }
