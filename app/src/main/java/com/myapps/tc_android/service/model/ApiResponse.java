9835b39ed9cbc17bd263db0d4689033009ea74df
Mhmd77 <m.emami9@yahoo.com>
2018-12-19 23:04:01 +0330

    fix: Change api response class to support all responses

--- app/src/main/java/com/myapps/tc_android/service/model/ApiResponse.java
+++ app/src/main/java/com/myapps/tc_android/service/model/ApiResponse.java
@@ -1,23 +1,51 @@
 package com.myapps.tc_android.service.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 
-public class ApiResponse<T>{
-    @SerializedName("status")
-    private String status;
+public class ApiResponse<T> {
     @SerializedName("object")
     private T object;
+    @SerializedName("token")
+    private String token;
+    @SerializedName("status")
+    private String status;
 
-    public ApiResponse(String status, T user) {
-        this.status = status;
-        this.object = user;
+    public String getToken() {
+        return token;
+    }
+
+    public T getObject() {
+        return object;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public T getObject() {
-        return object;
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void parseObject(JsonElement json) {
+        if (json == null) {
+            setObject(null);
+        }
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.enableComplexMapKeySerialization().create();
+        T parsedObject = gson.fromJson(json, new TypeToken<T>() {
+        }.getType());
+        setObject(parsedObject);
     }
 }
