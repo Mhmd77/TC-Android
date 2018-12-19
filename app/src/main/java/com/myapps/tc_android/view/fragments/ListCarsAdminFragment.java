d66641749b4151c02742e92beee81df4283e380c
Mhmd77 <m.emami9@yahoo.com>
2018-12-19 22:46:51 +0330

    fix: Remove unused parts of code from list cars admin activity

--- app/src/main/java/com/myapps/tc_android/view/fragments/ListCarsAdminFragment.java
+++ app/src/main/java/com/myapps/tc_android/view/fragments/ListCarsAdminFragment.java
@@ -4,46 +4,31 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.myapps.tc_android.R;
-
-import com.myapps.tc_android.view.adapter.CarsRecyclerView;
-import com.myapps.tc_android.service.repository.ApiService;
-import com.myapps.tc_android.service.repository.ApiRepository;
-import com.myapps.tc_android.service.model.ApiResponse;
 import com.myapps.tc_android.service.model.Car;
 import com.myapps.tc_android.view.activities.CarProfileActivity;
+import com.myapps.tc_android.view.adapter.CarsRecyclerView;
 import com.myapps.tc_android.viewmodel.ListCarsViewModel;
-
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class ListCarsAdminFragment extends Fragment implements CarsRecyclerView.UserOnItemClickListener {
     @BindView(R.id.recyclerView_main_cars)
     RecyclerView recyclerViewMainCars;
 
-//    @BindView(R.id.actionButton_main_add_car)
-    // private FloatingActionButton actionButtonMainAddCar = (FloatingActionButton) getActivity().findViewById(R.id.actionButton_main_add_car);
-
     private CarsRecyclerView adapter;
-    private ApiService service;
 
     private Unbinder unbinder;
     private ListCarsViewModel viewModel;
@@ -70,13 +55,6 @@ public class ListCarsAdminFragment extends Fragment implements CarsRecyclerView.
                 generateDataList(cars);
             }
         });
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
     }
 
     @Override
