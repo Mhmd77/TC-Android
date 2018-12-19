5f7c900c291492276a2bf7877330252f39d929c8
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-19 14:15:59 +0330

    feat: Add location for rent car object

--- app/src/main/java/com/myapps/tc_android/model/RentCarObject.java
+++ app/src/main/java/com/myapps/tc_android/model/RentCarObject.java
@@ -1,43 +1,38 @@
 package com.myapps.tc_android.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
 import java.sql.Date;
 
-public class RentCarObject {
+public class RentCarObject implements Serializable {
+    @SerializedName("id")
+    private int id;
+    @SerializedName("car_id")
     private int carId;
+    @SerializedName("user_id")
     private int userId;
+    @SerializedName("start_date")
     private Date startDate;
+    @SerializedName("end_date")
     private Date endDate;
+    @SerializedName("source_location")
+    private int srcLocation;
+    @SerializedName("destination_location")
+    private int desLocation;
 
-    public int getCarId() {
-        return carId;
-    }
 
-    public void setCarId(int carId) {
+    public RentCarObject(int id, int carId, int userId, Date startDate, Date endDate, int srcLocation, int desLocation) {
+        this.id = id;
         this.carId = carId;
-    }
-
-    public void setUserId(int userId) {
         this.userId = userId;
-    }
-
-    public void setStartDate(Date startDate) {
         this.startDate = startDate;
-    }
-
-    public void setEndDate(Date endDate) {
         this.endDate = endDate;
+        this.srcLocation = srcLocation;
+        this.desLocation = desLocation;
     }
 
-    public int getUserId() {
-        return userId;
-    }
 
-    public Date getStartDate() {
-        return startDate;
-    }
 
-    public Date getEndDate() {
-        return endDate;
-    }
 
 }
