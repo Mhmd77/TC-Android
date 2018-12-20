ef1ad57995043bca3c1a89ef4ef17df96b56bcaf
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-20 18:36:46 +0330

    feat: Add onclick for reserve button

--- app/src/main/java/com/myapps/tc_android/view/activities/CarProfileActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/CarProfileActivity.java
@@ -9,16 +9,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.myapps.tc_android.R;
-import com.myapps.tc_android.service.repository.ApiService;
-import com.myapps.tc_android.service.repository.ApiRepository;
-import com.myapps.tc_android.service.model.ApiResponse;
 import com.myapps.tc_android.service.model.Car;
 import com.myapps.tc_android.service.model.UserHolder;
+import com.myapps.tc_android.service.repository.ApiRepository;
+import com.myapps.tc_android.service.repository.ApiService;
 import com.myapps.tc_android.viewmodel.CarViewModel;
 import com.squareup.picasso.Picasso;
 
@@ -26,9 +26,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class CarProfileActivity extends AppCompatActivity {
 
@@ -54,6 +51,8 @@ public class CarProfileActivity extends AppCompatActivity {
     ImageButton buttonDeleteCarProfile;
     @BindView(R.id.imageview_car_logo)
     CircleImageView imageviewCarLogo;
+    @BindView(R.id.button_reserve)
+    Button buttonReserve;
     private CarViewModel viewModel;
 
     @Override
@@ -69,6 +68,8 @@ public class CarProfileActivity extends AppCompatActivity {
         if (UserHolder.Instance().getUser().getRole().equals("admin") == false) {
             buttonEditCarprofile.setVisibility(View.GONE);
             buttonDeleteCarProfile.setVisibility(View.GONE);
+            buttonReserve.setVisibility(View.VISIBLE);
+
         }
     }
 
@@ -101,7 +102,7 @@ public class CarProfileActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.button_edit_carprofile, R.id.button_delete_car_profile})
+    @OnClick({R.id.button_edit_carprofile, R.id.button_delete_car_profile, R.id.button_reserve})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.button_edit_carprofile:
@@ -113,6 +114,12 @@ public class CarProfileActivity extends AppCompatActivity {
             case R.id.button_delete_car_profile:
                 LiveData<Boolean> liveData = ApiRepository.getInstance().deleteCar(carId);
                 obserDeleteRequest(liveData);
+                break;
+            case R.id.button_reserve:
+                Intent i = new Intent(CarProfileActivity.this, RentCarActivity.class);
+                i.putExtra("Car", car);
+                startActivity(i);
+                finish();
                 break;
         }
     }
