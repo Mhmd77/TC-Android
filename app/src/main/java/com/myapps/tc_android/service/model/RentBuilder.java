41dcfed6e99e2150c1c6953fada5c20d5bfebb01
bardia_kzzzz <bardiakarimi77@yahoo.com>
2018-12-21 12:56:22 +0330

    feat: Add total location and total cost to the rent builder

--- app/src/main/java/com/myapps/tc_android/service/model/RentBuilder.java
+++ app/src/main/java/com/myapps/tc_android/service/model/RentBuilder.java
@@ -1,30 +1,29 @@
 package com.myapps.tc_android.service.model;
 
 
-
 import java.sql.Date;
 
 public class RentBuilder {
     private int id;
     private int carId;
-    private int userId;
     private Date startDate;
     private Date endDate;
     private int srcLocation;
     private int desLocation;
+    private int cost;
+    private int kilometer;
 
-    public RentBuilder setRentId(int rentId) {
-        this.id = rentId;
+    public RentBuilder setCost(int cost){
+        this.cost = cost;
+        return this;
+    }
+    public RentBuilder setKilometer(int kilometer){
+        this.kilometer = kilometer;
         return this;
     }
 
     public RentBuilder setCarId(int carId) {
         this.carId = carId;
-        return this;
-    }
-
-    public RentBuilder setUserId(int userId) {
-        this.userId = userId;
         return this;
     }
 
@@ -48,8 +47,8 @@ public class RentBuilder {
         return this;
     }
 
-    public RentCarObject createRent() {
-        return new RentCarObject(id, carId, userId, startDate, endDate, srcLocation, desLocation);
+    public RentCar createRent() {
+        return new RentCar(id, carId, startDate, endDate, srcLocation, desLocation,cost,kilometer);
     }
 
 
