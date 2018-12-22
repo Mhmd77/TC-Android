932456a7e3f5312c3368c837cd07eff3d36f5799
Mhmd77 <m.emami9@yahoo.com>
2018-12-22 23:00:32 +0330

    fix: Change list cars view model logic

--- app/src/main/java/com/myapps/tc_android/viewmodel/ListCarsViewModel.java
+++ app/src/main/java/com/myapps/tc_android/viewmodel/ListCarsViewModel.java
@@ -14,33 +14,22 @@ import java.util.List;
 public class ListCarsViewModel extends ViewModel {
     private MutableLiveData<List<Car>> listCarsObservableData;
 
-    public ListCarsViewModel(boolean isUserCar) {
-        if (isUserCar) {
-            this.listCarsObservableData = ApiRepository.getInstance().getListCarUser();
-        } else {
-            this.listCarsObservableData = ApiRepository.getInstance().getListCars();
-        }
+    public ListCarsViewModel() {
+        this.listCarsObservableData = new MutableLiveData<>();
     }
 
     public LiveData<List<Car>> getListCarsObservableData() {
         return listCarsObservableData;
     }
 
+    public void getCars(){
+        ApiRepository.getInstance().getListCars(listCarsObservableData);
+    }
     public void sortListOfCars(String field, int ascending) {
         ApiRepository.getInstance().sortListCars(listCarsObservableData, field, ascending);
     }
 
-    public static class Factory extends ViewModelProvider.NewInstanceFactory {
-        private boolean isUserCar;
-
-        public Factory(boolean isUserCar) {
-            this.isUserCar = isUserCar;
-        }
-
-        @NonNull
-        @Override
-        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-            return (T) new ListCarsViewModel(isUserCar);
-        }
+    public void getCarsUser() {
+        ApiRepository.getInstance().getListCarUser(listCarsObservableData);
     }
 }
