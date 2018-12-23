ae8acf53b472b31a237fb410e8cd2c59182144cd
Mhmd77 <m.emami9@yahoo.com>
2018-12-23 22:50:55 +0330

    fix: Change add car admin logic
    Setting user id
    Remove unused libraries
    Debug add car observe's method

--- app/src/main/java/com/myapps/tc_android/view/activities/AddCarAdminActivity.java
+++ app/src/main/java/com/myapps/tc_android/view/activities/AddCarAdminActivity.java
@@ -8,12 +8,12 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -23,9 +23,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.myapps.tc_android.R;
-import com.myapps.tc_android.service.model.CarBuilder;
-import com.myapps.tc_android.service.model.ApiResponse;
 import com.myapps.tc_android.service.model.Car;
+import com.myapps.tc_android.service.model.CarBuilder;
+import com.myapps.tc_android.service.model.UserHolder;
 import com.myapps.tc_android.viewmodel.CarViewModel;
 import com.myapps.tc_android.viewmodel.PostImageViewModel;
 
@@ -40,9 +40,6 @@ import in.mayanknagwanshi.imagepicker.imagePicker.ImagePicker;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class AddCarAdminActivity extends AppCompatActivity {
 
@@ -93,7 +90,8 @@ public class AddCarAdminActivity extends AppCompatActivity {
                     .setKilometer(Integer.parseInt(editTextAddCarKilometer.getText().toString()))
                     .setPrice(Integer.parseInt(editTextAddCarPrice.getText().toString()))
                     .setYear(Integer.parseInt(editTextAddCarYear.getText().toString()))
-                    .setAutomate(editTextAddCarAutomate.isChecked());
+                    .setAutomate(editTextAddCarAutomate.isChecked())
+                    .setUserId(UserHolder.Instance().getUser().getId());
             viewModel.addCar(builder.createCar());
         }
     }
@@ -104,8 +102,12 @@ public class AddCarAdminActivity extends AppCompatActivity {
             public void onChanged(@Nullable Car recievedCar) {
                 if (recievedCar != null) {
                     car = recievedCar;
-                    if (image != null)
+                    if (image != null) {
                         sendPhoto();
+                    } else {
+                        finish();
+                        startActivity(new Intent(AddCarAdminActivity.this, HomePageAdminActivity.class));
+                    }
                 }
             }
         });
