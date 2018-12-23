e1421165c3341c2dc2832a14db12fef1797cb04a
Mhmd77 <m.emami9@yahoo.com>
2018-12-23 22:14:07 +0330

    fix: Change car view model logic

--- app/src/main/java/com/myapps/tc_android/viewmodel/CarViewModel.java
+++ app/src/main/java/com/myapps/tc_android/viewmodel/CarViewModel.java
@@ -11,45 +11,16 @@ import com.myapps.tc_android.service.repository.ApiRepository;
 
 public class CarViewModel extends ViewModel {
     private final MutableLiveData<Car> carObservableData;
-    private int carId;
 
-    public CarViewModel(int carId) {
-        this.carId = carId;
-        carObservableData = ApiRepository.getInstance().getCar(carId);
-    }
-
-    public CarViewModel(Car car) {
-        carObservableData = ApiRepository.getInstance().addCar(car);
+    public CarViewModel() {
+        carObservableData = new MutableLiveData<>();
     }
 
     public LiveData<Car> getCarObservableData() {
         return carObservableData;
     }
 
-    public void updateCar() {
-        ApiRepository.getInstance().getCar(carObservableData, carId);
-    }
-
-    public static class Factory extends ViewModelProvider.NewInstanceFactory {
-        private int carId;
-        private Car car;
-
-        public Factory(int carId) {
-            this.carId = carId;
-        }
-
-        public Factory(Car car) {
-            this.car = car;
-        }
-
-        @NonNull
-        @Override
-        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-            if (car == null) {
-                return (T) new CarViewModel(carId);
-            } else {
-                return (T) new CarViewModel(car);
-            }
-        }
+    public void addCar(Car car) {
+        ApiRepository.getInstance().addCar(carObservableData, car);
     }
 }
