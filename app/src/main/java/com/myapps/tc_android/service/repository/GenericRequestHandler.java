9c411e34d9954ca65997c607656f380ebd077e53
Mhmd77 <m.emami9@yahoo.com>
2018-12-23 22:24:53 +0330

    fix: Remove unused part of code from generic request handler

--- app/src/main/java/com/myapps/tc_android/service/repository/GenericRequestHandler.java
+++ app/src/main/java/com/myapps/tc_android/service/repository/GenericRequestHandler.java
@@ -11,29 +11,6 @@ import retrofit2.Response;
 public abstract class GenericRequestHandler<R> {
     abstract protected Call<ApiResponse<R>> makeRequest();
 
-    public final MutableLiveData<R> doRequest() {
-        final MutableLiveData<R> liveData = new MutableLiveData<>();
-        makeRequest().enqueue(new ApiCallback<R>() {
-            @Override
-            protected void handleResponseData(R data) {
-                liveData.setValue(data);
-            }
-
-            @Override
-            protected void handleError(Response<ApiResponse<R>> response) {
-                Log.e("Request Error", "Error failed with code: " + response.code() + " and message : " + response.message());
-                liveData.setValue(null);
-            }
-
-            @Override
-            protected void handleException(Exception t) {
-                t.printStackTrace();
-                liveData.setValue(null);
-            }
-        });
-        return liveData;
-    }
-
     public final void doRequest(final MutableLiveData<R> liveData) {
         makeRequest().enqueue(new ApiCallback<R>() {
             @Override
