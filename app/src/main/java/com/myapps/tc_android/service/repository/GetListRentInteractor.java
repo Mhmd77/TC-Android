d5dea4859577712229865dad8fbdceffda67be87
unknown <bardiakarimi77@yahoo.com>
2019-01-27 17:40:03 +0330

    feat: Add interceptor for getting list of rent

--- app/src/main/java/com/myapps/tc_android/service/repository/GetListRentInteractor.java
+++ app/src/main/java/com/myapps/tc_android/service/repository/GetListRentInteractor.java
@@ -1,4 +1,16 @@
 package com.myapps.tc_android.service.repository;
 
-public class GetListRentInteractor {
+import com.myapps.tc_android.service.model.ApiResponse;
+import com.myapps.tc_android.service.model.Car;
+import com.myapps.tc_android.service.model.RentCar;
+
+import java.util.List;
+
+import retrofit2.Call;
+
+public class GetListRentInteractor extends GenericRequestHandler<List<RentCar>> {
+    @Override
+    protected Call<ApiResponse<List<RentCar>>> makeRequest() {
+        return ApiRepository.apiService.getRentUser();
+    }
 }
